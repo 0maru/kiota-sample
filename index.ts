a620e815045843ed5422f657dcaf8107e3d0d2f2
Toi Yoshimaru <0maru.dev@gmail.com>
2024-02-14 23:19:31 +0900

    リクエストが通るところまで書いた

--- index.ts
+++ index.ts
@@ -1,5 +1,20 @@
+import { AnonymousAuthenticationProvider } from "@microsoft/kiota-abstractions"
+import { FetchRequestAdapter } from "@microsoft/kiota-http-fetchlibrary"
+import { createApiClient } from "./client/apiClient"
+
+const authProvider = new AnonymousAuthenticationProvider()
+const adapter = new FetchRequestAdapter(authProvider)
+adapter.baseUrl = "http://localhost:3000"
+
+const client = createApiClient(adapter)
+
 const main = async () => {
     console.log('run index.ts')
+
+    const response = await client.contact.post({
+        message: 'test',
+    })
+    console.log(response)
 }
 
 main()
